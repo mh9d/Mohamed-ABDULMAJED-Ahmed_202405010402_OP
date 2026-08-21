0735cb1ce2fa8b1a5b7e677241b06dc4c9eae016
mh9d <vjtnnj7k2d@privaterelay.appleid.com>
2026-08-21 04:40:54 +0000

    week_7_done

---
+++ Week_6/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        lecturer l = new lecturer("L101", "Dr majed","OOP", "Faculty of Information Technology");
+        l.displayInfo();
+        l.displaySubject();
+        l.displayDepartment();
+    }
