c35743b93d1964d90825d4c0b4f46571c0c52756
mh9d <vjtnnj7k2d@privaterelay.appleid.com>
2026-08-21 05:21:51 +0000

    work done

--- Week_6/main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Lecturer l = new Lecturer("L101", "Dr majed", "OOP", "Faculty of Information Technology");
-        l.displayInfo();
-        l.displaySubject();
-        l.displayDepartment();
-    }
-}
